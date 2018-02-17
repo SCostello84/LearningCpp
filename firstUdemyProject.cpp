b7071af58dd26d846e2e92cef9b066c60536bad0
Stephen Costello <Scorch010@gmail.com>
2018-02-16 20:36:42 -0500

    Create firstUdemyProject.cpp

---
+++ firstUdemyProject.cpp
@@ -0,0 +1,15 @@
+// firstUdemyProject.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <cstdio>
+
+int main()
+{
+	std::cout << "Hello World\n";
+
+	std::getchar();
+    return 0;
+}
+
